2b5caaaf617fdb6539963f7968a6fdc09ef36fbd
Wesley Gomes <falecomweslley@gamil.com>
2017-02-14 13:54:47 -0300

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,53 @@
-$(function(){
-	var frase = $(".frase").text();
-	var numeropalavra = frase.split(" ").length;
-	$("#tamanhoFrase").text(numeropalavra);
 
-	$(".campo-digitacao").on('input', function(){
+	var tempoInicial = $("#tempo-digitacao").text();
 
-		var conteudo = $(".campo-digitacao").val();
+	$(function(){
 
-		var qdtPalavras =  conteudo.split(/\S+/).length -1;
-		$("#contato-palavras").text(qdtPalavras);
-		$("#contato-caracteres").text(conteudo.length);
+		atualizaTamanhoFrase();
+		inicializaContadores();
+		inicializaCronometro();
+		$("#btnReiniciar").click(reiniciaJogo);
+
 	});
+
+	function atualizaTamanhoFrase(){	
+		var frase = $(".frase").text();
+		var numeropalavra = frase.split(" ").length;
+		$("#tamanhoFrase").text(numeropalavra);
+	}
+
+	function inicializaContadores(){
+		$(".campo-digitacao").on('input', function(){
+			var conteudo = $(".campo-digitacao").val();
+			var qdtPalavras =  conteudo.split(/\S+/).length -1;
+
+			$("#contato-palavras").text(qdtPalavras);
+			$("#contato-caracteres").text(conteudo.length);
+		});
+	}
 	
-});
+	function inicializaCronometro(){
+		var tempoRestante = $("#tempo-digitacao").text();
+		$(".campo-digitacao").one('focus', function(){
+			var cronometroID = setInterval(function(){
+				tempoRestante--;
+				$("#tempo-digitacao").text(tempoRestante);
+				if(tempoRestante < 1){
+					$(".campo-digitacao").attr('disabled', true);
+					clearInterval(cronometroID);
+					$(".campo-digitacao").addClass("campoDisable");
+				}
+			}, 1000);
+		});
+	}
+
+	function reiniciaJogo(){
+		$(".campo-digitacao").attr('disabled', false);
+		$(".campo-digitacao").val("");
+		$("#contato-palavras").text("0");
+		$("#contato-caracteres").text("0");
+		$("#tempo-digitacao").text(tempoInicial);
+		inicializaCronometro();
+	}
+
+	
